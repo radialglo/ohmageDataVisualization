10207d4536dfb071742468a81ff11c7a15ed6d59
Anthony Su <antchovee@gmail.com>
2012-11-10 14:49:39 -0800

    updated spacing

--- demo/data_demo.js
+++ demo/data_demo.js
@@ -8,37 +8,37 @@
 
 $(document).ready(function(){
 
-	var defaultTimeout = 600; //in milliseconds
+        var defaultTimeout = 600; //in milliseconds
 
-	$.ohmg.login("cs130.07","Clean.order31");
+        $.ohmg.login("cs130.07","Clean.order31");
 
-	setTimeout(function(){
+        setTimeout(function(){
 
-		//wait until authentication is complete before getting token
+                //wait until authentication is complete before getting token
 
-		var auth_token = $.ohmg.getToken();
+                var auth_token = $.ohmg.getToken();
 
-		$.ohmg.getCampaigns(auth_token,"ohmage-js","long");
-	
-	}, defaultTimeout);
+                $.ohmg.getCampaigns(auth_token,"ohmage-js","long");
+        
+        }, defaultTimeout);
 
-	setTimeout(function(){
-		//wait until authentication is complete before getting token
+        setTimeout(function(){
+                //wait until authentication is complete before getting token
 
-		var auth_token = $.ohmg.getToken()
-		  , optional = {prompt_id_list : "urn:ohmage:special:all"};//prompt_id_list
-		  
-		$.ohmg.getSurveyResponse(
-		 			  auth_token
-				    , "urn:campaign:ca:ucla:Demo:Snack" //campaign_urn
-					, undefined //client_type
-					, "urn:ohmage:special:all" //column_list:
-					, "json-rows" //output_format
-					, "urn:ohmage:special:all" //user_list
-					, optional
-		);
+                var auth_token = $.ohmg.getToken()
+                  , optional = {prompt_id_list : "urn:ohmage:special:all"};//prompt_id_list
+                  
+                $.ohmg.getSurveyResponse(
+                                          auth_token
+                                    , "urn:campaign:ca:ucla:Demo:Snack" //campaign_urn
+                                        , undefined //client_type
+                                        , "urn:ohmage:special:all" //column_list:
+                                        , "json-rows" //output_format
+                                        , "urn:ohmage:special:all" //user_list
+                                        , optional
+                );
 
-	}, defaultTimeout);
+        }, defaultTimeout);
 
 
 });
