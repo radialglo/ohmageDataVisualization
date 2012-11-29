aacbc454dd669f03dc9ff3428a35341da88a45bf
YJ Jin <yeon.joon.jin+gh1@gmail.com>
2012-11-28 22:43:02 -0800

    TimeSeries: Data Processing + Timeline
    added files to test: timeSeriesDemo.html and application_stub.js

--- js/timeseries/ohmg_timeSeries.js
+++ js/timeseries/ohmg_timeSeries.js
@@ -1,1 +1,59 @@
 // JavaScript Document
+      google.load('visualization', '1', {'packages':['annotatedtimeline']});
+      
+var TimeSeries = {
+
+      //Draws chart given a processed Table as parameter
+      //Code is modified version from https://developers.google.com/chart/interactive/docs/gallery/annotatedtimeline
+      drawChart: function (my_table) 
+      {
+        var data = new google.visualization.DataTable();
+        data.addColumn('date', 'Date');
+        data.addColumn('number', 'Responses');
+        data.addRows(my_table);
+        var chart = new google.visualization.AnnotatedTimeLine(document.getElementById('chart_div'));
+        chart.draw(data, {displayAnnotations: false});
+      },
+
+      //Invoked outside of class with data being the response from the Ohmage Server
+      //the data will be exactly what the Ohmage Server returned.
+      //we go through the data.data (the array that contains the responses, sorted in date order)
+      // counting the number of times a response occured per day.
+      setSurveyResponse: function (data)
+      {
+        var surveyResponseTable = data.data;
+        var processedTable= [];
+        if(surveyResponseTable.length == 0) return TimeSeries.rawChart(processedTable);
+        var lastDate = surveyResponseTable[0].date;
+        var dateCount = 0;
+        var currentDate;
+        var j = 0;
+        for(var i = 0; i < surveyResponseTable.length; i++)
+        {
+          currentDate = surveyResponseTable[i].date;
+          if(lastDate === currentDate)
+          {
+            dateCount++;
+          }
+          else
+          {
+            processedTable[j] = TimeSeries.pairToRow(lastDate, dateCount);
+            j++;
+            lastDate = currentDate;
+            dateCount = 1;
+          }
+        }
+        processedTable[j] = TimeSeries.pairToRow(lastDate, dateCount);
+        TimeSeries.drawChart(processedTable);
+      },
+
+      //given a dateString formatted "YYYY-MM-DD" (the format from the response)
+      //      and a number (representing the number of responses corresponding to that date)
+      // return a row in an array that has [date,number] (format required by the datatable)
+      pairToRow: function (dateString, number)
+      {
+        var dates = dateString.split("-");
+        return [new Date(dates[0], dates[1]-1, dates[2]), number];
+      }
+
+}
