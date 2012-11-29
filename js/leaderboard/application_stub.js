58d7834447e9fb7607ff2d00a540fb31380ba1fa
Shuang Xu <shuangxu@ucla.edu>
2012-11-28 23:43:14 -0800

    Added Leaderboard demo
    demo

---
+++ js/leaderboard/application_stub.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+var defaultTimeout = 1500;
+
+
+
+
+$.ohmg.login("cs130.07","Clean.order31");
+
+
+setTimeout(function() 
+{
+var auth_token = $.ohmg.getToken();
+var optional = {prompt_id_list : "urn:ohmage:special:all"
+                    , sort_order : "timestamp,user,survey"}; //setting sort order by timestamp
+
+$.ohmg.getSurveyResponse(
+                             auth_token
+                          , "urn:campaign:ca:ucla:Demo:Snack" //campaign_urn
+                          , undefined //client_type ; if client is not defined it defaults to ohmage-js
+                          , "urn:ohmage:special:all" //column_list:
+                           , "json-rows" //output_format
+                           , "urn:ohmage:special:all" //user_list
+                           , optional
+                           , function(surveyResponseTable){
+                               Leaderboard.drawTable(surveyResponseTable);
+                             });
+    
+  }, defaultTimeout);
+
+});
