afdf2eb8f5643802e6b3ccb5340d4a81a8e2f270
Anthony Su <antchovee@gmail.com>
2012-11-30 01:55:12 -0800

    added display for individual marker when clicked, added .css to style map

--- demo/map_demo.js
+++ demo/map_demo.js
@@ -12,19 +12,26 @@
        var optional = {prompt_id_list : "urn:ohmage:special:all"};//prompt_id_list 
 
           
-		$.ohmg.getSurveyResponse(
-		 			data.token
-				    , "urn:campaign:ca:ucla:Demo:Snack" //campaign_urn
-					, undefined //client_type
-					, "urn:ohmage:special:all" //column_list:
-					, "json-rows" //output_format
-					, "urn:ohmage:special:all" //user_list
-					, optional
-                    , getSurveyInfo
-		);
-     }
+        $.ohmg.getSurveyResponse(
+                    data.token
+                    , "urn:campaign:ca:ucla:Demo:Snack" //campaign_urn
+                    , undefined //client_type
+                    , "urn:ohmage:special:all" //column_list:
+                    , "json-rows" //output_format
+                    , "urn:ohmage:special:all" //user_list
+                    , optional
+            , getSurveyInfo
+        );
+    }
 
  );
+
+ var map = undefined,
+    $markerInfo = $('<div/>');
+    $markerInfo.attr("id","mapInfoBox")
+               .html("Info");
+
+
 
 
  function getSurveyInfo(obj) {
@@ -46,30 +53,45 @@
     };
 
 
-    var map = new google.maps.Map(document.getElementById("ohmg_map"), options);
+    map = new google.maps.Map(document.getElementById("ohmg_map"), options);
 
     map.fitBounds(mapBounds);
 
     var markers = [];
+   
 
-
-	for(var i = 0 ; i < surveyResponses.length; i++ ) {
+    for(var i = 0 ; i < surveyResponses.length; i++ ) {
 
       response = surveyResponses[i];
 
-   	  //$('body').append("<p>"+ JSON.stringify(response) + "</p><br/>");
-     
-      //console.log("latitude: " + response.latitude + " longititude: " + response.longitude);
       
       if(response.latitude && response.longitude) {
          var latLng = new google.maps.LatLng(response.latitude,
                response.longitude);
-         var marker = new google.maps.Marker({'position': latLng});
-             markers.push(marker);
+         var marker = new google.maps.Marker({'animation': google.maps.Animation.DROP,'position': latLng});
+             
+         //closure to encapsulate data
+         (function(m,r) {
+           google.maps.event.addListener(marker, 'click', function() {
+                displayPoint(m,r);
+            }) 
+         })(marker,response);
+
+
+         markers.push(marker);
        }
+
    }
 
+   
    var markerCluster = new MarkerClusterer(map, markers);
+   google.maps.event.addListener(markerCluster, 'clusterclick', function() {
+      $markerInfo.hide();
+   });
+  
+   google.maps.event.addListener(map, 'zoom_changed', function() {
+      $markerInfo.hide();
+   });
    
 }
 
@@ -98,28 +120,81 @@ function centerMapAndFitMarkers(map,latlngbounds) {
 
 }
 
+//ElementOverlay inspired from http://marcgrabanski.com/article/jquery-google-maps-tutorial-basics
+
+function displayPoint(marker,response){ 
 
 
-/*
-function computeCenter(arr) {
-
-    var lat = 0, lng = 0, count = 0;
-
-    for(var i = 0; i < arr.length; i++) {
-
-        if(arr[i].latitude && arr[i].longitude) {
-
-            lat += arr[i].latitude;
-            lng += arr[i].longitude;
-            count +=1;
-        }
-    }
-
-   return  {
-         latitude : lat/count
-       , longitude: lng/count
-   
-   };
+  $markerInfo.hide(); 
 
 
-} */
+   var moveEnd = google.maps.event.addListener(map, "idle" , function() {
+
+      var messageOverlay = new ElementOverlay( { map: map }, $markerInfo[0], marker.getPosition(), response );
+
+      messageOverlay.draw();
+
+          
+
+      google.maps.event.removeListener(moveEnd);
+
+  });
+
+
+  map.panTo(marker.getPosition()); 
+  
+}
+
+
+function ElementOverlay(options, overlayedObject, point,response)   {
+
+    this.setValues(options);
+
+    this.div_ = overlayedObject;
+
+    this.div_point_ = point;
+
+    this.response_ = response;
+
+};
+
+
+
+ElementOverlay.prototype = new google.maps.OverlayView();
+
+
+
+ElementOverlay.prototype.onAdd = function() {
+
+    var pane = this.getPanes().floatPane; //panel 6 used for InfoBox
+
+    pane.appendChild(this.div_);
+
+
+    // This is how to get pixels
+
+    var curOffset = this.getProjection().fromLatLngToDivPixel(this.div_point_);
+
+        
+
+    $(this.div_).text("")
+      .html(  "<ul>"
+            + "<li><span>User:</span>" + this.response_.user + "</li>"
+            + "<li><span>Date:</span>" + this.response_.timestamp.split(" ")[0] + "</li>"
+            + "<li><span>Time:</span>" + this.response_.timestamp.split(" ")[1] + "</li>"
+            + "<li><span>Privacy:</span>" + this.response_.privacy_state + "</li>"
+            + "</ul>" )
+      .fadeIn().css({ top: (curOffset.y ), left: (curOffset.x) } )
+      .click(function(){ $(this).fadeOut(); });
+
+}
+
+
+
+ElementOverlay.prototype.onRemove = function() {
+
+    this.div_.parentNode.removeChild(this.div_);
+
+}
+
+ElementOverlay.prototype.draw = function() {}
