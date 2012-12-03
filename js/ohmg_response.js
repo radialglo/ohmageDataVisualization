d7c4571269966e05415bde6c5f06559d75ee8fb8
Shuang Xu <shuangxu@ucla.edu>
2012-12-02 19:48:21 -0800

    response update, and demo leaderboard updated
    Leaderboard updated to use new renamed files
    Response has been broken down so that it's easier to insert campaign
    name

--- js/ohmg_response.js
+++ js/ohmg_response.js
@@ -15,14 +15,16 @@ var Response = {
 			if(survey[i].prompt_type == 'photo' && survey[i].prompt_response != "SKIPPED"){
 				console.log(survey[i].prompt_response);
 				
-				var image_link = 'https://test.ohmage.org/app/image/read';
-				//var image_link = $.ohmg.url.root + "/" + $.ohmg.url.image_read;
+				//var image_link = 'https://test.ohmage.org/app/image/read';
+				var image_link = $.ohmg.url.root + "/" + $.ohmg.url.image_read;
 				
 				image_link+= '?id=' + survey[i].prompt_response
-				image_link+= '&amp;camaign_urn=urn:campaign:ca:ucla:Demo:Snack&amp;auth_token=' + $.ohmg.getToken()
+				image_link+= '&amp;camaign_urn=';
+				image_link+= 'urn:campaign:ca:ucla:Demo:Snack';
+				image_link+= '&amp;auth_token=' + $.ohmg.getToken()
 				image_link+= '&amp;owner=' + tname + '&amp;client=android&amp;size=small';
 
-				console.log(image_link)
+				//console.log(image_link)
 				rdata.addRows([[
 				//https://test.ohmage.org/app/image/read?id=c4710b56-93d2-4a3a-a7fc-1c6d45b04988&amp;campaign_urn=urn:campaign:ca:ucla:Demo:Advertisement&amp;auth_token=d3540b47-61bb-421a-bf7f-56961316dd0e&amp;owner=mobilize.student&amp;client=ohmage-gwt&amp;size=small" class="GHKF5YNDE0B
 					survey[i].prompt_text, '<img src="' +image_link + '">'
